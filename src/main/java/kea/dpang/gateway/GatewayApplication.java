b482df7369254abaefe4750ac21fc5e88e313869
Seok-Hun <hamer1233@naver.com>
2024-01-23 17:15:50 +0900

    Feat: JWT AccessToken 확인 기능 추가 토큰을 secreet key로 파싱하여 확인하고 올바른 값이면 토큰 내의 내용을 request. Refresh 토큰 확인 기능 추가 필요. 서비스로 보낼 request header 확인 필요. 이후 쿠버네티스에 빌드하여 올렸을 때 연결할 서비스들의 routes 확인 필요(application.propterties)

--- src/main/java/kea/dpang/gateway/GatewayApplication.java
+++ src/main/java/kea/dpang/gateway/GatewayApplication.java
@@ -1,7 +1,15 @@
 package kea.dpang.gateway;
 
+import kea.dpang.gateway.handler.GlobalExceptionHandler;
+import org.apache.http.HttpHeaders;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -10,4 +18,13 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+    @Bean
+    public ErrorWebExceptionHandler globalExceptionHandler(){
+        return new GlobalExceptionHandler();
+    }
+
+    @Bean
+    public KeyResolver tokenKeyResolver(){
+        return exchange -> Mono.just(exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0));
+    }
 }
---
+++ src/main/java/kea/dpang/gateway/component/AuthorizationHeaderFilter.java
@@ -0,0 +1,75 @@
+package kea.dpang.gateway.component;
+
+import kea.dpang.gateway.security.JwtTokenProvider;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    public AuthorizationHeaderFilter(JwtTokenProvider jwtTokenProvider) {
+        super(Config.class);
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    static class Config {
+
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            log.info("AuthorizationHeaderFilter Start: request -> {}", exchange.getRequest());
+
+            HttpHeaders headers = request.getHeaders();
+            if (!headers.containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(exchange, "No authorization header", HttpStatus.UNAUTHORIZED);
+            }
+
+            String authorizationHeader = headers.get(HttpHeaders.AUTHORIZATION).get(0);
+
+            // JWT 토큰 판별
+            String token = authorizationHeader.substring(7);
+
+
+            jwtTokenProvider.validateJwtToken(token);
+
+            String subject = jwtTokenProvider.getUserId(token);
+
+            if (!jwtTokenProvider.getRoles(token).contains("admin")) {
+                return onError(exchange, "권한 없음", HttpStatus.UNAUTHORIZED);
+            }
+
+            ServerHttpRequest newRequest = request.mutate()
+                    .header("X-DPANG-Token", subject)
+                    .build();
+
+            log.info("AuthorizationHeaderFilter End");
+            return chain.filter(exchange.mutate().request(newRequest).build());
+        };
+    }
+
+    // Mono(단일 값), Flux(다중 값) -> Spring WebFlux
+    private Mono<Void> onError(ServerWebExchange exchange, String errorMsg, HttpStatus httpStatus) {
+        log.error(errorMsg);
+
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+
+        return response.setComplete();
+    }
+}
